31d2693cf4165c13d72e26bb18f64729a48b8543
Dmitrii Kozlov <mitya.mitya.dima@gmail.com>
2023-12-25 22:24:59 +0300

    вносит правки после созвона в задание 4ого модуля

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,6 @@
-/* в этот файл добавляет скрипты*/
+const menuBtn = document.querySelector('.main-nav__toggle');
+const menuMobile = document.querySelector('.main-nav__wrapper');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('main-nav__wrapper--active');
+});
