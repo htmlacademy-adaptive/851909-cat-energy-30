d9e441b29b457f03f67f2745a19b1c559f25d0d7
Dmitrii Kozlov <mitya.mitya.dima@gmail.com>
2024-01-16 18:24:42 +0300

    добавляет сетки для главной страницы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,7 @@
-const menuBtn = document.querySelector('.main-nav__toggle');
-const menuMobile = document.querySelector('.main-nav__wrapper');
+const menuBtn = document.querySelector('.header__toggle');
+const menuMobile = document.querySelector('.main-nav');
 
 menuBtn.addEventListener('click', () => {
-  menuMobile.classList.toggle('main-nav__wrapper--active');
+  menuBtn.classList.toggle('is-open');
+  menuMobile.classList.toggle('is-open');
 });
